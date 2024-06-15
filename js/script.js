79fc1cd98b9178b089d5ae5893b15502b7cc06a1
SavandiNarmada <savandi.narmada@gmail.com>
2024-06-15 08:43:55 +0530

    change words

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ window.onscroll = () => {
 
 window.addEventListener('DOMContentLoaded', () => {
     const typingText = document.querySelector('.typing');
-    const skills = ['Undergrauate Student','UI/UX Designer', 'Front-end Developer', 'ICT Tutor'];
+    const skills = ['Undergraduate Student','UI/UX Designer', 'Front-end Developer', 'ICT Tutor'];
   
     let index = 0;
     let charIndex = 0;
